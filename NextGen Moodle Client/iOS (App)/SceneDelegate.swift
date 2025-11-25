84f251c66a0b8807761eafa7a2b2a8a2e7f3d65d
Luca Sain <luca.sain@outlook.com>
2025-11-25 19:15:00 -0300

    working on macos safari by running this xcrun safari-web-extension-converter ./dist (on a computer with xcode)

---
+++ NextGen Moodle Client/iOS (App)/SceneDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  SceneDelegate.swift
+//  iOS (App)
+//
+//  Created by Luca on 25/11/2025.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+
+}
